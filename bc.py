a17d26ca1e26e41468cdbc363d01e7c2d3fd3534
NguyenThanhCong170 <142860612+NguyenThanhCong170@users.noreply.github.com>
2023-12-09 13:19:47 +0700

    Update bc.py

--- bc.py
+++ bc.py
@@ -5,4 +5,6 @@ import cv2
 # cap = cv2.imread('abc1.jpg')
 reader = easyocr.Reader(['en'], gpu=False)
 result = reader.readtext('/abc.jpg', detail = 0)
+output = "".join(result)
+output = output.upper().replace(' ',"-").replace('*',"-")
 print(result)
