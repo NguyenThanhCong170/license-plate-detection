468ee63cf1cce7c7d785c6fb80426000623c20e3
NguyenThanhCong170 <142860612+NguyenThanhCong170@users.noreply.github.com>
2023-12-09 22:59:04 +0700

    Update bc.py

--- bc.py
+++ bc.py
@@ -1,10 +1,25 @@
 import easyocr
-import cv2
+import csv
+import os
 
-
-# cap = cv2.imread('abc1.jpg')
-reader = easyocr.Reader(['en'], gpu=False)
-result = reader.readtext('/abc.jpg', detail = 0)
-output = "".join(result)
-output = output.upper().replace(' ',"-").replace('*',"-")
-print(result)
+def OCR_va_CSV(image):
+  reader = easyocr.Reader(['en'], gpu=False)
+  result = reader.readtext(image, detail = 0)
+  output = "".join(result)
+  output = output.upper().replace(' ',"-").replace('*',"-")
+  print(result)
+  
+  #nhớ điều chỉnh file mình muốn ở đâu
+  filepath = ".\detect.csv"
+  if not os.path.isfile(filepath):
+  
+      file = open('detect.csv', 'w', newline='')
+      csv_writer = csv.writer(file)
+      csv_writer.writerow([f'output'])
+      file.close()
+  else:
+      file = open('detect.csv', 'a', newline='')
+      csv_writer = csv.writer(file)
+      csv_writer.writerow([f'output'])
+      file.close()
+    
